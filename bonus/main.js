853d419dca309445405e1252ff9897059010eb76
Deni0012 <deni.dg00@icloud.com>
2025-03-04 16:14:11 +0100

    feat: aggiunta di commenti e function nel js

--- bonus/main.js
+++ bonus/main.js
@@ -1,1 +1,17 @@
-console.log('ciao Denise')
+// js style
+const lamp = document.getElementById("lamp");
+const button = document.querySelector("button");
+
+
+
+function toggleLight() {
+    if (lamp.src.includes("white_lamp.png")) {
+        lamp.src = "img/yellow_lamp.png";
+        button.textContent = "Premi per spegnere"
+    } else {
+        lamp.src = "img/white_lamp.png";
+        button.textContent = "Premi per accendere"
+    }
+}
+
+button.addEventListener("click", toggleLight);
