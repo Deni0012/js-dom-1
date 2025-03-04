d9d1e78e131f10f695a026acb304c30a4091ddb2
Deni0012 <deni.dg00@icloud.com>
2025-03-04 16:09:01 +0100

    feat: img e js function

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('ciao Denise');
+// js style
+
+const lamp = document.getElementById(`lamp`);
+const button = document.querySelector("button");
+
+
+
+function lightOn() {
+    lamp.src = "img/yellow_lamp.png";
+}
+
+button.addEventListener("click", lightOn);
